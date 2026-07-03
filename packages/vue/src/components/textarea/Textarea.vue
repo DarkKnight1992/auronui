7ff6498bdeb067e552895164cf47384784e1a18f
Suraj Sudhera <suraj.sudhera@gmail.com>
2026-07-03 10:08:38 +0800

    refactor: extract Textarea.vue's form-field state machine into useFormField

--- packages/vue/src/components/textarea/Textarea.vue
+++ packages/vue/src/components/textarea/Textarea.vue
@@ -24,6 +24,9 @@ import { useTextareaAutosize } from '@vueuse/core'
 import { textAreaVariants, type TextAreaVariants } from '@auronui/styles'
 import { composeClassName , type ClassValue} from '../../utils/composeClassName'
 import { useDeprecatedBooleanProp } from '../../composables/useDeprecatedBooleanProp'
+import { useFormField } from '../../composables/useFormField'
+import FieldLabel from '../_shared/FieldLabel.vue'
+import FormFieldHelper from '../_shared/FormFieldHelper.vue'
 
 defineOptions({ inheritAttrs: false })
 
@@ -127,18 +130,29 @@ if (props.autoResize) {
   })
 }
 
-const hasLabel = computed(() => !!props.label)
 const isFilled = computed(() => modelValue.value != null && String(modelValue.value) !== '')
 
-const descriptionId = computed(() => `${inputId.value}-description`)
-const errorMessageId = computed(() => `${inputId.value}-error`)
-const showError = computed(() => props.isInvalid && !!props.errorMessage)
-const showDescription = computed(() => !!props.description && !showError.value)
-const hasHelper = computed(() => showError.value || showDescription.value)
-const ariaDescribedBy = computed(() => {
-  if (showError.value) return errorMessageId.value
-  if (showDescription.value) return descriptionId.value
-  return undefined
+const {
+  descriptionId,
+  errorMessageId,
+  showError,
+  showDescription,
+  hasHelper,
+  ariaDescribedBy,
+  hasLabel,
+  showOutsideLabel,
+  showInsideLabel,
+  rootDataAttrs,
+} = useFormField({
+  fieldId: () => inputId.value,
+  label: () => props.label,
+  description: () => props.description,
+  errorMessage: () => props.errorMessage,
+  isInvalid: () => props.isInvalid,
+  isDisabled: () => props.isDisabled,
+  isReadOnly: () => isReadOnly.value,
+  isRequired: () => props.isRequired,
+  labelPlacement: () => props.labelPlacement,
 })
 
 const isInteractive = computed(() => !props.isDisabled && !isReadOnly.value)
@@ -165,47 +179,33 @@ const slotFns = computed(() =>
     labelPlacement: props.labelPlacement,
   }),
 )
-
-const showOutsideLabel = computed(
-  () => hasLabel.value && props.labelPlacement !== 'inside',
-)
-const showInsideLabel = computed(
-  () => hasLabel.value && props.labelPlacement === 'inside',
-)
 </script>
 
 <template>
   <div
     :class="composeClassName(slotFns.base(), props.class, props.classNames?.base)"
-    :data-invalid="isInvalid || undefined"
-    :data-disabled="isDisabled || undefined"
-    :data-readonly="isReadOnly || undefined"
-    :data-required="isRequired || undefined"
-    :data-has-label="hasLabel || undefined"
-    :data-has-helper="hasHelper || undefined"
+    v-bind="rootDataAttrs"
   >
-    <label
+    <FieldLabel
       v-if="showOutsideLabel"
       :for="inputId"
+      :label="label"
+      :is-required="isRequired"
       :class="composeClassName(slotFns.label(), props.classNames?.label)"
-    >{{ label }}<span
-      v-if="isRequired"
-      aria-hidden="true"
-    > *</span></label>
+    />
 
     <div :class="composeClassName(slotFns.mainWrapper(), props.classNames?.mainWrapper)">
       <div
         :class="composeClassName(slotFns.inputWrapper(), props.classNames?.inputWrapper)"
         :data-filled="hasLabel ? (isFilled || undefined) : undefined"
       >
-        <label
+        <FieldLabel
           v-if="showInsideLabel"
           :for="inputId"
+          :label="label"
+          :is-required="isRequired"
           :class="composeClassName(slotFns.label(), props.classNames?.label)"
-        >{{ label }}<span
-          v-if="isRequired"
-          aria-hidden="true"
-        > *</span></label>
+        />
         <span
           v-if="$slots.startContent"
           :class="composeClassName(slotFns.startContent(), props.classNames?.startContent)"
@@ -272,25 +272,18 @@ const showInsideLabel = computed(
         </button>
       </div>
 
-      <div
-        v-if="hasHelper"
-        :class="composeClassName(slotFns.helperWrapper(), props.classNames?.helperWrapper)"
-      >
-        <div
-          v-if="showError"
-          :id="errorMessageId"
-          :class="composeClassName(slotFns.errorMessage(), props.classNames?.errorMessage)"
-        >
-          {{ errorMessage }}
-        </div>
-        <div
-          v-else-if="showDescription"
-          :id="descriptionId"
-          :class="composeClassName(slotFns.description(), props.classNames?.description)"
-        >
-          {{ description }}
-        </div>
-      </div>
+      <FormFieldHelper
+        :has-helper="hasHelper"
+        :show-error="showError"
+        :show-description="showDescription"
+        :error-message="errorMessage"
+        :description="description"
+        :error-message-id="errorMessageId"
+        :description-id="descriptionId"
+        :wrapper-class="composeClassName(slotFns.helperWrapper(), props.classNames?.helperWrapper)"
+        :error-class="composeClassName(slotFns.errorMessage(), props.classNames?.errorMessage)"
+        :description-class="composeClassName(slotFns.description(), props.classNames?.description)"
+      />
     </div>
   </div>
 </template>
